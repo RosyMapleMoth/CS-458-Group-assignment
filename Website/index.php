18e715946ace78b044320dddd6e47a02d53c6c7a
cryopshinx <55608339+cryopshinx@users.noreply.github.com>
2019-09-23 17:34:01 -0700

    More website stuff

--- Website/index.php
+++ Website/index.php
@@ -4,26 +4,23 @@
 	<head>
 		<title> Travel Humboldt </title>
 		<meta charset="utf-8" />
-		
-    <link href=
-        "http://nrs-projects.humboldt.edu/~st10/styles/normalize.css" type="text/css" rel="stylesheet" />
+			
+		<link href="css/normalize.css" type="text/css" rel="stylesheet" />
 
-    <link href="css/style.css" type="text/css"  rel="stylesheet" />
-	
-	<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<script src="nav.js"></script>
+		<link href="css/style.css" type="text/css"  rel="stylesheet" />
+		
+		<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 
 <?php
 	require_once("header.html");
 ?>
 
-	<div class="content">
-	Look at me, pretending to be a fancy designer with my Latin. 
-	
-	 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rutrum suscipit lacus sit amet ultrices. Proin mauris quam, malesuada ut tincidunt in, commodo scelerisque mi. Nullam posuere neque sed augue tincidunt, a imperdiet erat tristique. Pellentesque sodales pulvinar metus. Quisque efficitur tincidunt congue. Phasellus id ante scelerisque, ullamcorper ipsum vitae, eleifend orci. Vestibulum tristique justo ut tempus bibendum. Integer pretium neque nisi, vitae pulvinar metus fermentum vel. Vivamus commodo sollicitudin suscipit. Suspendisse id dictum massa. Vivamus finibus sem ante, eu lobortis massa condimentum eu. Nulla sagittis eros nec eros tincidunt ullamcorper. Duis aliquet nisl non dolor sollicitudin consectetur. Sed ipsum tortor, consequat vel fringilla nec, tempus vel risus. Nunc malesuada, nunc vel dictum efficitur, odio nibh viverra ligula, in cursus ex mauris eu enim.
+	<div id="content" class="content">
+		<!-- Content div is controlled by jQuery, and will load PHP files based on the what the user click -->
+		<script src="js/jquery-3.3.1.min.js"></script>
+		<script src="js/nav.js"></script>
 	</div>
 
 	<!-- Footer -->
@@ -31,10 +28,10 @@
 <?php
 	require_once("footer.html");
 ?>
-
+	<!-- Used for switching the navigation between full top nav and hamburger icon -->
 	<script>
 		function myFunction() {
-		  var x = document.getElementById("myTopnav");
+		  var x = document.getElementById("topnav");
 		  if (x.className === "topnav") {
 			x.className += " responsive";
 		  } else {
