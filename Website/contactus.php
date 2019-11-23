b8f57612ea6384b40483eab97a09f63e3ec3be8d
cryopshinx <55608339+cryopshinx@users.noreply.github.com>
2019-11-22 17:24:36 -0800

    Update-ContactUS
    Mostly made the contactus.php and contactus_submit.php functional.
    Also added .jpg proof that it's working and what hte e-mail looks like.
    
    Also, added a txt file for a good way to pick a random row from a SQL table

--- Website/contactus.php
+++ Website/contactus.php
@@ -8,7 +8,15 @@
 		<link href="css/normalize.css" type="text/css" rel="stylesheet" />
 
 		<link href="css/style.css" type="text/css"  rel="stylesheet" />
-		
+
+<script src="https://www.google.com/recaptcha/api.js?render=6LfeNMQUAAAAAAq1ZRT5UBd9TskftWMX4Wvefjif"></script>
+<script>
+grecaptcha.ready(function() {
+    grecaptcha.execute('6LfeNMQUAAAAAAq1ZRT5UBd9TskftWMX4Wvefjif', {action: 'homepage'}).then(function(token) {
+       ...
+    });
+});
+</script>	
 		<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
@@ -18,7 +26,53 @@
 ?>
 
 	<div id="content" class="content">
-			Contact
+		<form action="contactus_submit.php" method="POST">
+				
+			<div id="form">
+				<fieldset>
+					<legend> Event Information </legend>
+						<label>Name:
+							<input type="text" name="name" required="required" maxlength="25" size="25" />
+						</label>
+						<br /><br />
+						<label>Email:
+							<input type="text" name="email" required="required" maxlength="25" size="25" />
+						</label>
+						<br /><br />
+						<label>Comment/Message:
+							<textarea name="comment" required="required" rows="5" cols="25" /></textarea>
+						</label>
+						<br /><br />
+				</fieldset>
+				<br/><br/>
+				<fieldset>
+					<legend> Are we missing something awesome? Please let us know! </legend>
+						<label> What type of something is this?
+							<select name="type">
+								<option value="blank" selected="selected">Please select...</option>
+								<option value="dest">Destination</option>
+								<option value="eatery">Eatery</option>
+								<option value="ev">Event</option>
+								<option value="att">Attraction</option>
+							</select>
+						</label>
+						<br /><br />
+						<label>Tell us all about it!:
+							<textarea name="description" required="required" rows="5" cols="25" /></textarea>
+						</label>
+						<br/><br/>
+				</fieldset>
+			</div>
+		
+			<div class="g-recaptcha" data-sitekey="6LfeNMQUAAAAAAq1ZRT5UBd9TskftWMX4Wvefjif">
+			</div>      
+			<br/>
+			
+			<fieldset>
+				<input type="submit" value="Submit">
+				<input type="reset" value="Clear" />
+			</fieldset>
+		</form>	
 	</div>
 
 	<!-- Footer -->
---
+++ Website/contactus_submit.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title> Find Your Humboldt - Feeling Random?</title>
+		<meta charset="utf-8" />
+			
+		<link href="css/normalize.css" type="text/css" rel="stylesheet" />
+
+		<link href="css/style.css" type="text/css"  rel="stylesheet" />
+		
+		<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+
+<?php
+	require_once("header.html");
+?>
+
+<?php
+	#input from contactus.php form
+	$name = 'Name: ' . $_POST['name'];
+	$email = $_POST['email'];
+	$comment = 'Comment box: ' . $_POST['comment'];
+	
+	$type = 'Suggestion type: ' . $_POST['type'];
+	$description = 'Suggestion description: ' . $_POST['description'];
+	
+	#Filter input of user e-mail
+	#Filter user input
+	function filter_email_header($form_field) {
+		return preg_replace('/[\0\n\r\|\!\/\<\>\^\$\%\*\&]+/','',$form_field);
+	}
+	
+	#Reassign e-mail with filtered function
+	$email = 'Email: ' . filter_email_header($email);
+
+	
+	#Send e-mail
+	$to = 'mlg40@humboldt.edu';
+	$subject = 'Contact Us form from Find Your Humboldt';
+	$message = "You have received a form from the Contact Us page of Find Your Humboldt" . "\r\n" . $name . "\r\n" . $email . "\r\n" . $comment . "\r\n" . $type . "\r\n" . $description;
+	$headers = 'Contact Us form from Find Your Humboldt';
+	$sent = mail($to, $subject, $message, $headers);
+
+	#If form is successfully sent, produce 'thank you'
+	#Otherwise, show that is was unsuccessful
+	if ($sent) 
+	{
+		?>
+		<h1>Thank You</h1>
+		<p>Thank you for your feedback.</p>
+
+		<?php
+
+	} 
+	else {
+		?>
+		<h1>Something went wrong</h1>
+		<p>We could not send your feedback. Please try again.</p>
+		<?php
+	}
+?>
+
+<!-- Footer -->
+<?php
+	require_once("footer.html");
+?>
+	<!-- Used for switching the navigation between full top nav and hamburger icon -->
+	<script>
+		function myFunction() {
+		  var x = document.getElementById("topnav");
+		  if (x.className === "topnav") {
+			x.className += " responsive";
+		  } else {
+			x.className = "topnav";
+		  }
+		}
+	</script>
+	</body>
+</html>
+
