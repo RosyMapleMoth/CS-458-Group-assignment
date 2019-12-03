33bd62e2d3ec8f76664466ecea4f9245b4b6ae7e
Seaney Shell <sfs185@humboldt.edu>
2019-12-02 18:55:28 -0800

    Added names to bizPop

--- bizPop.sql
+++ bizPop.sql
@@ -3,7 +3,7 @@ insert into MLG40.Biz_prof
 values(1,'Frankies NY Bagels', 'edwoods 3750 Harris St',
                 'Eureka','CA','Eatery','y','7075993305',null,null,'');
 insert into MLG40.Eatery
-values (1,'Frankie’s NY Bagels has brought the crunchy outside, soft and chewy inside: classic, New York Style Bagel to Humboldt County.',
+values (1,'Frankies NY Bagels','Frankie’s NY Bagels has brought the crunchy outside, soft and chewy inside: classic, New York Style Bagel to Humboldt County.',
  'https://www.facebook.com/frankiesnybagels/', '$','n','y','n',1,'edwoods 3750 Harris St', 'Eureka', 'CA');
 
 
@@ -11,7 +11,7 @@ insert into MLG40.Biz_prof
 values(2,'Cafe Brio', '791 G St',
                 'Arcata','CA','Eatery','y','7078225922',null,null,'');
 insert into MLG40.Eatery
-values (2,'Cafe Brio is a casual all-day cafe located in Arcata California',
+values (2,'Cafe Brio','Cafe Brio is a casual all-day cafe located in Arcata California',
  'https://www.cafebrioarcata.com/', '$$','n','y','n',2,'791 G St', 'Arcata', 'CA');
 
 
@@ -19,7 +19,7 @@ insert into MLG40.Biz_prof
 values(3,'Tonis Restaurant', '1901 Heindon Rd',
                 'Arcata','CA','Eatery','y','7078220091',null,null,'');
 insert into MLG40.Eatery
-values (3,'Round-the-clock eatery serving a large variety of breakfast options, burgers, shakes and homemade pie.',
+values (3,'Tonis Restaurant'.'Round-the-clock eatery serving a large variety of breakfast options, burgers, shakes and homemade pie.',
  'https://tonisrestaurant.com/', '$$','n','y','n',3,'1901 Heindon Rd', 'Arcata', 'CA');
 
 
@@ -27,7 +27,7 @@ insert into MLG40.Biz_prof
 values(6,'The Big Blue Cafe', '846 G St',
                 'Arcata','CA','Eatery','y','7075085175',null,null,'');
 insert into MLG40.Eatery
-values (6,'You’ll find a warm, cozy atmosphere with delicious, fresh meals served up with the Big Blue’s own special blend of organic house coffee. The coffee is always strong, and the food is made from scratch using only the best ingredients.',
+values (6,'The Big Blue Cafe','You’ll find a warm, cozy atmosphere with delicious, fresh meals served up with the Big Blue’s own special blend of organic house coffee. The coffee is always strong, and the food is made from scratch using only the best ingredients.',
  'http://humboldtdining.com/big-blue-cafe/', '$$','n','y','n',6,'846 G St', 'Arcata', 'CA');
 
 
@@ -35,7 +35,7 @@ insert into MLG40.Biz_prof
 values(7,'Los Bagels', '1085 I St #101',
                 'Arcata','CA','Eatery','y','7078223483',null,null,'');
 insert into MLG40.Eatery
-values (7,'Rustic cafe offering house-baked bagels, breads and pastries, along with Mexican treats.',
+values (7,'Los Bagels','Rustic cafe offering house-baked bagels, breads and pastries, along with Mexican treats.',
  'http://losbagels.com/', '$','n','y','n',7,'1085 I St #101', 'Arcata', 'CA');
 
 
@@ -43,7 +43,7 @@ insert into MLG40.Biz_prof
 values(8,'Cafe Phoenix', '1360 G St',
                 'Arcata','CA','Eatery','y','7076305021',null,null,'');
 insert into MLG40.Eatery
-values (8,'',
+values (8,'Cafe Phoenix','no discription found',
  'http://www.facebook.com/cafephoenixarcata', '$$','n','y','n',8,'1360 G St', 'Arcata', 'CA');
 
 
@@ -51,7 +51,7 @@ insert into MLG40.Biz_prof
 values(9,'Alibi', '744 Ninth St',
                 'Arcata','CA','Eatery','y','7078223731',null,null,'');
 insert into MLG40.Eatery
-values (9,'The Alibi menu is a variety of traditional home-style cooking; including soups, salads, wraps, sandwiches, famous burgers, full dinners, and the always popular breakfasts.',
+values (9,'Alibi','The Alibi menu is a variety of traditional home-style cooking; including soups, salads, wraps, sandwiches, famous burgers, full dinners, and the always popular breakfasts.',
  'http://www.facebook.com/cafephoenixarcata', '$$','n','y','n',9,'1360 G St', 'Arcata', 'CA');
 
 
@@ -60,7 +60,7 @@ insert into MLG40.Biz_prof
 values(10,'Moonstone Crossing Winery', '1000 Moonstone Cross Rd',
                 'Trinidad','CA','Eatery','y','7076773832',null,null,'');
 insert into MLG40.Attraction
-values (10,'Winery','$$', '7076773832', 
+values (10,'Moonstone Crossing Winery','Winery','$$', '7076773832', 
  'http://www.facebook.com/cafephoenixarcata', '$$','n','y','n',9,'1360 G St', 'Arcata', 'CA');
 
 
@@ -68,7 +68,7 @@ insert into MLG40.Biz_prof
 values(11,'Humboldt Brews LLC', '856 Tenth St',
                 'Loleta','CA','Eatery','y','7078262739',null,null,'');
 insert into MLG40.Eatery
-values (11,'Bar and eatery with a rotating selection of local brews on tap and pub fare, including vegetarian items.',
+values (11,'Humboldt Brews LLC','Bar and eatery with a rotating selection of local brews on tap and pub fare, including vegetarian items.',
  'http://www.humbrews.com/', '$$','n','y','n',11,'856 Tenth St', 'Arcata', 'CA');
 
 
@@ -76,7 +76,7 @@ insert into MLG40.Biz_prof
 values(14,'Dons Donuts Pizza Deli', '933 H St',
                 'Arcata','CA','Eatery','y','7078226465',null,null,'');
 insert into MLG40.Eatery
-values (14,'A small donut and Pizza shop open 24/7',
+values (14,'Dons Donuts Pizza Deli','A small donut and Pizza shop open 24/7',
  'no URL', '$','n','y','n',14,'933 H St', 'Arcata', 'CA');
 
 
@@ -84,7 +84,7 @@ insert into MLG40.Biz_prof
 values(15,'Eel River Brewing Co.', '1777 Alamar Way',
                 'Fortuna','CA','Eatery','y','7077252739',null,null,'');
 insert into MLG40.Eatery
-values (15,'Eco-conscious brewpub featuring organic craft beer, hearty bar fare and a large patio with horseshoes.',
+values (15,'Eel River Brewing Co.','Eco-conscious brewpub featuring organic craft beer, hearty bar fare and a large patio with horseshoes.',
  'http://www.eelriverbrewing.com/', '$$','n','y','n',15,'1777 Alamar Way', 'Fortuna', 'CA');
 
 
@@ -92,7 +92,7 @@ insert into MLG40.Biz_prof
 values(16,'Subway', '1731 G St Suite A',
                 'Arcata','CA','Eatery','y','7078220155',null,null,'');
 insert into MLG40.Eatery
-values (16,'Casual counter-serve chain for build-your-own sandwiches and salads, with health-conscious options.',
+values (16,'Subway','Casual counter-serve chain for build-your-own sandwiches and salads, with health-conscious options.',
  'https://order.subway.com/en-us/restaurant/5835/', '$','n','y','n',16,'1731 G St Suite A', 'Arcata', 'CA');
 
 
@@ -100,7 +100,7 @@ insert into MLG40.Biz_prof
 values(17,'Ramones Bakery and Cafe', '209 E St', 
                 'Eureka','CA','Eatery','y','7074426313',null,null,'');
 insert into MLG40.Eatery
-values (17,'Stop by for a morning cup of coffee with a breakfast pastry, a mid-day lunch or an afternoon cup of tea and a treat at any of our bakery locations!',
+values (17,'Ramones Bakery and Cafe','Stop by for a morning cup of coffee with a breakfast pastry, a mid-day lunch or an afternoon cup of tea and a treat at any of our bakery locations!',
  'http://ramonesbakery.com/', '$$','n','y','n',17,'209 E St', 'Eureka', 'CA');
 
 
@@ -108,7 +108,7 @@ insert into MLG40.Biz_prof
 values(19,'Northtown Coffee', '1603 G St',
                 'Arcata','CA','Eatery','y','7076377277',null,null,'');
 insert into MLG40.Eatery
-values (19,'Small coffe shope in Arcata',
+values (19,'Northtown Coffee','Small coffe shope in Arcata',
            'http://www.organicnorthtown.com/', '$$','n','y','n',19,'1603 G St', 'Arcata', 'CA');
 
 
@@ -116,7 +116,7 @@ insert into MLG40.Biz_prof
 values(21,'Papa Murphys Pizza', '1940 Central Avenue',
                 'McKinleyville','CA','Eatery','y','7078397200',null,null,'');
 insert into MLG40.Eatery
-values (21,'Chain outlet offering build-your-own takeaway pizzas, to be cooked at home.',
+values (21,'Papa Murphys Pizza','Chain outlet offering build-your-own takeaway pizzas, to be cooked at home.',
  'url too big', '$','n','y','y',21,'1940 Central Avenue', 'McKinleyville', 'CA');
 
 
@@ -124,7 +124,7 @@ insert into MLG40.Biz_prof
 values(22,'Everetts Club', '784 9th St',
                 'Arcata','CA','Eatery','y','7078222291',null,null,'');
 insert into MLG40.Eatery
-values (22,'',
+values (22,'Everetts Club','no dsicription is given',
  '', '','n','y','n',22,'784 9th St', 'Arcata', 'CA');
 
 
@@ -133,7 +133,7 @@ insert into MLG40.Biz_prof
 values(24,'Abruzzi', '780 7th St',
                 'Arcata','CA','Eatery','y','7078262376',null,null,'');
 insert into MLG40.Eatery
-values (24,'Traditional Italian dining and wines in a rustic former storehouse with low lighting and stone walls',
+values (24,'Abruzzi','Traditional Italian dining and wines in a rustic former storehouse with low lighting and stone walls',
  'abruzziarcata.com', '$$$','n','y','n',24,'780 7th St', 'Arcata', 'CA');
 
 
@@ -141,7 +141,7 @@ insert into MLG40.Biz_prof
 values(25,'Libation Wine Bar', '761 Eighth St Suite A',
                 'Arcata','CA','Eatery','y','7078251755',null,null,'');
 insert into MLG40.Eatery
-values (25,'Wines by the glass, beer on tap, small plates and full retail wine shop.',
+values (25,'Libation Wine Bar','Wines by the glass, beer on tap, small plates and full retail wine shop.',
  'https://www.facebook.com/libationarcata/', '$$','n','y','n',25,'761 Eighth St Suite A', 'Arcata', 'CA');
 
 
@@ -149,7 +149,7 @@ insert into MLG40.Biz_prof
 values(26,'The Wildflower Cafe', '1604 G St',
                 'Arcata','CA','Eatery','y','7078220360',null,null,'');
 insert into MLG40.Eatery
-values (26,'Compact, homey eatery featuring locally sourced vegetarian meals, including breakfast, since 1987.',
+values (26,'The Wildflower Cafe','Compact, homey eatery featuring locally sourced vegetarian meals, including breakfast, since 1987.',
  'http://wildflowercafebakery.com/', '$$','n','y','n',26,'1604 G St', 'Arcata', 'CA');
 
 
@@ -157,7 +157,7 @@ insert into MLG40.Biz_prof
 values(27,'Philly CheeseSteak Shoppe', '1811 G St',
                 'Arcata','CA','Eatery','y','7078257400',null,null,'');
 insert into MLG40.Eatery
-values (27,'A local cheese stake shop in the arcata area, part of a small chain.',
+values (27,'Philly CheeseSteak Shoppe','A local cheese stake shop in the arcata area, part of a small chain.',
  'http://www.phillycs.com/', '$$','n','y','n',9,'1811 G St', 'Arcata', 'CA');
 
 
@@ -165,62 +165,62 @@ insert into MLG40.Biz_prof
 values(10,'Moonstone Crossing Winery', '1000 Moonstone Cross Rd',
                 'Trinidad','CA','Eatery','y','7076773832',null,null,'');
 insert into MLG40.Attraction
-values (10,'Winery','$$', '7076773832', '1000 Moonstone Cross Rd','Trinidad', 'CA', 'Moonstone Crossing wines are made from select grapes grown in small, hillside vineyards from the Napa, Sonoma, Mendocino, and Amador appellations. Moonstone Crossing wines are hand-crafted in small quantities using traditional-style winemaking techniques.',10)
+values (10,'Moonstone Crossing Winery','Winery','$$', '7076773832', '1000 Moonstone Cross Rd','Trinidad', 'CA', 'Moonstone Crossing wines are made from select grapes grown in small, hillside vineyards from the Napa, Sonoma, Mendocino, and Amador appellations. Moonstone Crossing wines are hand-crafted in small quantities using traditional-style winemaking techniques.',10)
 
 
 insert into MLG40.Biz_prof
 values(4,'Bear River Casino', '11 Bear Paws Way',
                 'Loleta','CA','Eatery','y','7077339644',null,null,'');
 insert into MLG40.Attraction
-values (4,'Casino','$$$', '7077339644','11 Bear Paws Way', 'Loleta', 'ca' 'Go for the win at the slot machines and table games you most want to play! Open 24 hours a day, 365 days a year for your convenience!  Also, play while your away to win even more rewards!', 4)
+values (4,'Bear River Casino','Casino','$$$', '7077339644','11 Bear Paws Way', 'Loleta', 'ca' 'Go for the win at the slot machines and table games you most want to play! Open 24 hours a day, 365 days a year for your convenience!  Also, play while your away to win even more rewards!', 4)
 
 
 insert into MLG40.Biz_prof
 values(20,'Cher-Ae Heights Casino', '27 Scenic Drive',
                 'Trinidad','CA','Eatery','y','7076773611',null,null,'');
 insert into MLG40.Attraction
-values (20,'Casino','$$$','7076773611', '27 Scenic Drive', 'Trinidad', 'CA','Gambling and nightlife complex offering table games, slots, concerts, dining, a lounge and ocean views.', 20);
+values (20,'Cher-Ae Heights Casino','Casino','$$$','7076773611', '27 Scenic Drive', 'Trinidad', 'CA','Gambling and nightlife complex offering table games, slots, concerts, dining, a lounge and ocean views.', 20);
 
 
 insert into MLG40.Biz_prof
 values(18,'Arcata Theatre Lounge', '1036 G St',
                 'Arcata','CA','Eatery','y','7074994643',null,null,'');
 insert into MLG40.Attraction
-values (18,'Movie Theatre','$','7074994643','1036 G St', 'Arcata', 'CA','Small theatre shows old movies and hosts dance events' ,18);
+values (18,'Arcata Theatre Lounge','Movie Theatre','$','7074994643','1036 G St', 'Arcata', 'CA','Small theatre shows old movies and hosts dance events' ,18);
 
 
 insert into MLG40.Biz_prof
 values(5,'Wrangletown Cider', '1350 9th St',
                 'Arcata','CA','Eatery','y','7074419999',null,null,'');
 insert into MLG40.Attraction
-values (5,'Winery','$$','7074419999','1350 9th St','Arcata','CA','Wrangletown Cider Company produces, dry, farmhouse-style Ciders from small, family owned orchards in Humboldt County, CA.  Located a few blocks from the plaza in the Creamery District of Arcata.',5);
+values (5,'Wrangletown Cider','Winery','$$','7074419999','1350 9th St','Arcata','CA','Wrangletown Cider Company produces, dry, farmhouse-style Ciders from small, family owned orchards in Humboldt County, CA.  Located a few blocks from the plaza in the Creamery District of Arcata.',5);
 
 
 insert into MLG40.Outdoor_dest
-values(1,'y','18 acres','easy','Logging Museum','3431 Fort Ave',
+values(1,'Fort Humboldt State Historic Park','y','18 acres','easy','Logging Museum','3431 Fort Ave',
          'Eureka','CA','','Fort Humboldt is situated on a bluff overlooking Humboldt Bay.'
          ,'use address');
 
 
 insert into MLG40.Outdoor_dest
-values(2,'y','','hard','Hiking trail','Park Dr',
+values(2,'Redwoods park','y','','hard','Hiking trail','Park Dr',
          'Arcata','CA','','Park set amongst giant redwood trees with picnic tables, a lounge for events and small stage.'
           ,'use address');
 
 
 insert into MLG40.Outdoor_dest
-values(3,'y','','easy','Town center','844 H St',
-         'Arcata','CA','','Arcata plaza is a small park in the center of town, that hosts events often.'
+values(3,'Arcata plaza','y','','easy','Town center','844 H St',
+         'Arcata','CA','$$','Arcata plaza is a small park in the center of town, that hosts events often.'
          ,'use address');
 
 
 insert into MLG40.Outdoor_dest
-values(4,'y','','medium','Camp ground, Beach','Clam Beach Dr',
-         'McKinleyville','CA','','nice camp ground right off the Beach'
+values(4,'Clam Beach State Campground','y','','medium','Camp ground, Beach','Clam Beach Dr',
+         'McKinleyville','CA','$','nice camp ground right off the Beach'
          ,'use address');
 
 
 insert into MLG40.Outdoor_dest
-values(5,'n','307 acres','medium','Walking trails','Gearheart Marsh',
+values(5,'Gearheart Marsh','n','307 acres','medium','Walking trails','Gearheart Marsh',
          'Arcata','CA','','A wild life reserve with many walking trails'
          ,'use address');
