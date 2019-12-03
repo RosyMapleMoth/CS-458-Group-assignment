2d6b7c168cd070ee6055233d0412ea549e55eb37
Seaney Shell <sfs185@humboldt.edu>
2019-12-02 19:58:00 -0800

    added event.sql script this script can not be run until bizPop.sql has been run

---
+++ event.sql
@@ -0,0 +1,37 @@
+
+insert into MLG40.Event
+values(1,'Arts Alive', 'Street Fair',
+       'y','y','free','Arts alive is a street fair event where local artissins set up shop in down town Eureka, as well as many local eateraies offering discounts this ocurres the first saturday of every month',
+       '525 2nd St #105','Eureka','CA',null);
+
+
+
+insert into MLG40.Biz_prof
+values(100,'Blondies Food and Drink', '420 California Ave',
+                'Arcata','CA','Eatery','y','7074419999',null,null,'');
+insert into MLG40.Eatery
+values (100,'Blondies Food and Drink','A local cheese stake shop in the arcata area, part of a small chain.',
+ 'https://blondiesfoodanddrink.com/', '$$','n','y','n',100,'420 California Ave', 'Arcata', 'CA');
+
+insert into MLG40.Event
+values(2,'Science on Tap Presents: Futurama on Tap', 'Bar, Speeker',
+       'y','y','free','Good news everyone! This month’s installment of Science on Tap will explore the iconic show, Futurama, by Matt Groening and David Cohen, whom also wrote and created The Simpsons, The Tracey Ullman Show, and Disenchantment, just to name a few examples.',
+       '420 California Ave','Arcata','CA',100);
+
+
+
+insert into MLG40.Event
+values(3,'How The Grouch Stole ChristMurs', 'Music and show',
+       'y','y','$20','MURS The 12th and Final Tour of "How The Grouch Stole ChristMurs" Featuring The Grouch, Murs, Pure Powers and DJ Fresh
+21+ Ages',
+       '1036 G St','Arcata','CA',18);
+
+insert into MLG40.Event
+values(4,'Arts! Arcata', 'Street Fair',
+       'y','y','free','Arts! Arcata, produced by Arcata Main Street is a free, self-guided, public art phenomenon that brings art lovers and our community together every 2nd Friday of the month.',
+       '844 H St','Arcata','CA',null);
+
+insert into MLG40.Event
+values(5,'Sci-Fi Night: Battle of the Worlds (1961)', 'Music and show',
+       'y','y','5$ min','Sci-Fi Night: Battle of the Worlds (1961) Claude Rains stars as a mad scientist.',
+       '1036 G St','Arcata','CA',18);
